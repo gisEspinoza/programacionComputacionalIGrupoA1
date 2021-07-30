e3d8b50d9368f5f763afc73128820d6ec17be909
Gisela Espinoza <ggarcia@ugb.edu.sv>
2021-07-30 10:21:05 -0600

    Se agregar Ejercicio2, uso de switch y conversion de tipos

---
+++ clasePractica3007/Ejercicio2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clasePractica3007
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+            //declaracion de variables
+            int opcion;
+            double a, b;
+
+            Console.WriteLine("Escriba el valor de a: ");
+            a = Double.Parse( Console.ReadLine());
+            Console.WriteLine("Escriba el valor de b: ");
+            b = Double.Parse(Console.ReadLine());
+
+            //preguntar opciona usuario
+            Console.WriteLine("Seleccione l a operacion a realizar: 1-suma, 2-resta, 3-multiplicacion");
+            opcion = int.Parse(Console.ReadLine());
+
+
+             switch (opcion)
+             {
+                 case 1:
+                     Console.WriteLine(a + b);
+                     break;
+                 case 2:
+                     Console.WriteLine(a - b);
+                     break;
+                 case 3:
+                     Console.WriteLine(a * b);
+                     break;
+             }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
