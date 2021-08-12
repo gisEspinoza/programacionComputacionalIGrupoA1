27f42c9779e46753f6553059577c1abc50792bd6
Gisela Espinoza <ggarcia@ugb.edu.sv>
2021-08-12 07:54:51 -0600

    Se agrego proyecto para ejemplificar loops y arrays

---
+++ claseTeorica1208/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace claseTeorica1208
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //llamado al metodo loops
+            loops();
+
+            Console.ReadLine();
+        }
+
+        ///bucles
+        public static void loops()
+        {
+            int[] numbers = new int[3];
+            numbers[0] = 5;
+            numbers[1] = 10;
+            numbers[2] = 15;
+
+            //ciclo while
+            int i = 0;
+            Console.WriteLine("IMPRIME ELEMENTOS CON WHILE");
+            while (i < numbers.Length)
+            {
+                Console.WriteLine(numbers[i]);
+                i=i+1; //i++
+            }
+
+            Console.WriteLine("IMPRIME ELEMENTOS CON DO-WHILE");
+            int inc = 0;
+            do
+            {
+                Console.WriteLine(numbers[inc]);
+                inc++;
+            } while (inc < numbers.Length);
+
+
+            Console.WriteLine("IMPRIME ELEMENTOS CON FOR");
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                Console.WriteLine("Posicion {0} : {1}", j, numbers[j]);
+            }
+
+            Console.WriteLine("IMPRIME ELEMENTOS CON FOREACH");
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        
+    }
+}
